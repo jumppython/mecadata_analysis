18cdd4888797e37b1544bb405edbb4998ae46d98
jumppython <jumppython@gmail.com>
2018-01-07 00:16:14 +0900

    show data through json and canvasjs

--- pandas_test.py
+++ pandas_test.py
@@ -1,6 +1,7 @@
 from pandas import DataFrame, read_csv
 
 import matplotlib.pyplot as plt
+import matplotlib.font_manager as font_manager
 import pandas as pd
 import numpy as np
 import sys
@@ -11,10 +12,12 @@ print('Python version: '+sys.version)
 print('Pandas version: '+pd.__version__)
 print('Matplotlib version: '+matplotlib.__version__)
 
-fp = os.path.join(os.path.expanduser('~'),'DataAnalysis','mecadata','area_info.csv')
+bp = os.path.join(os.path.expanduser('~'),'DataAnalysis','mecadata')
+
+fp = os.path.join(bp,'area_info.csv')
 da = pd.read_csv(fp,names=['area_id','area_name','area_url'])
 
-fp = os.path.join(os.path.expanduser('~'),'DataAnalysis','mecadata','items_iteminfo_4.csv')
+fp = os.path.join(bp,'items_iteminfo_4.csv')
 df = pd.read_csv(fp)
 #price = df['price']
 #count, division = np.histogram(price,bins=[i for i in range(0,1000,2)],range=(0,100))
@@ -27,7 +30,29 @@ df = pd.read_csv(fp)
 
 ### count the main_type of each area and print out the top 10 of them ###
 for area_i in range(1,48):
+	areastr = 'area%d' % area_i
 	df_area = df[df['area_id']==area_i]
 	rs_area = df_area.groupby(['main_type'])['main_type'].count().reset_index(name='count').sort_values(['count'],ascending=False).head(10)
-	print da[da['area_id']==area_i]['area_name']
-	print rs_area[['main_type','count']].T
+	#print da[da['area_id']==area_i]['area_name']
+	#print rs_area[['main_type','count']].T
+	#fig = rs_area.plot.bar(x=['main_type'],y=['count'],alpha=0.3,figsize=(10,4)).get_figure()
+	#fig.savefig(os.path.join(bp,str(area_i)+'_area.png'))
+	#plt.close(fig)
+	rs_area.to_json(os.path.join(bp,'html','json',areastr+'_maintype.json'),orient='records')
+	maintype_i = 0
+	for maintype in rs_area['main_type']:
+		maintype_i += 1
+		maintypestr = 'maintype%d' % maintype_i
+		df_main = df_area[df_area['main_type']==maintype]
+		rs_main = df_main.groupby(['mid_type'])['mid_type'].count().reset_index(name='count').sort_values(['count'],ascending=False).head(5)
+		rs_main.to_json(os.path.join(bp,'html','json',areastr+'_'+maintypestr+'_midtype.json'),orient='records')
+		midtype_i = 0
+		for midtype in rs_main['mid_type']:
+			midtype_i += 1
+			midtypestr = 'midtype%d' % midtype_i
+			df_mid = df_main[df_main['mid_type']==midtype]
+			rs_mid = df_mid.groupby(['sub_type'])['sub_type'].count().reset_index(name='count').sort_values(['count'],ascending=False).head(3)
+			rs_mid.to_json(os.path.join(bp,'html','json',areastr+'_'+maintypestr+'_'+midtypestr+'_subtype.json'),orient='records')
+
+
+
